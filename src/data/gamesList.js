6be95dfb02d3630d3fb5184133d43bb0e75c9118
Atala <atala.rchidi@gmail.com>
2021-02-23 19:28:33 -0500

    added data game list

---
+++ src/data/gamesList.js
@@ -0,0 +1,32 @@
+const gamesList = [
+  {
+    id:1,
+    title:"The Last of Us",
+    genre:"Action-adventure",
+    developer:"Naughty Dog",
+    releaseDate:"Jul 29, 2014"
+  },
+  {
+    id:2,
+    title:"The Last of Us Part II",
+    genre:"Action-adventure",
+    developer:"Naughty Dog",
+    releaseDate:"Jun 19, 2020"
+  },
+  {
+    id:3,
+    title:"Marvel's Spider-Man",
+    genre:"Action-adventure",
+    developer:"Insomniac Games",
+    releaseDate:"Sep 7, 2018"
+  },
+  {
+    id:4,
+    title:"Marvel's Spider-Man: Miles Morales",
+    genre:"Action-adventure",
+    developer:"Insomniac Games",
+    releaseDate:"Nov 19, 2020"
+  }
+]
+
+export default gamesList;
